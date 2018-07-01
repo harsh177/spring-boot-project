0a3d1c6d5d1b877d5189cf9c990bda1316679428
Harsh Sharma <sharmaharsh177@gmail.com>
2018-07-01 16:44:29 +0530

    Flyway config added

--- src/main/java/com/messenger/dao/impl/MessagingDAOImpl.java
+++ src/main/java/com/messenger/dao/impl/MessagingDAOImpl.java
@@ -1,7 +1,10 @@
 package com.messenger.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.messenger.dao.interfaces.IMessagingDAO;
 
+@Repository
 public class MessagingDAOImpl implements IMessagingDAO{
 
 }
