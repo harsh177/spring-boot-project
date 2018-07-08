00bd9088f3719e2c902d81135ad22b3c59cabd4c
Harsh Sharma <sharmaharsh177@gmail.com>
2018-07-08 23:01:13 +0530

    video details example

--- src/main/java/com/messenger/MessengerApplication.java
+++ src/main/java/com/messenger/MessengerApplication.java
@@ -4,12 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.messenger.property.FileStorageProperties;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class MessengerApplication {
 
 	private static Logger logger = LoggerFactory.getLogger(MessengerApplication.class);
